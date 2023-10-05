cdd2788ecd54b3228c5067b9db306152a9ae876a
sarahecongu <sarahatimecongu@gmail.com>
2023-10-05 17:45:01 +0300

    added styles to the navbar styles

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-lg navbar-dark mb-5">
+<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container ">
       <!-- Logo on the left -->
       <a class="navbar-brand"  href="" >
